03cc1f397971f6dd5e69485176fba13567e91714
xhaqur xhariph <shakuritoshariff@gmail.com>
2024-07-01 11:38:00 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+let cards = [];
+
+function writeCards(){
+    for (let i = 0; i < names.length; i++){
+        cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    
+    }
+    return cards;
+}
+console.log(writeCards[cards]);
+
+function countDown(){
+    let num = 10 
+    while (num >= 0) {
+        console.log(num--);
+    }
+    return num;
+}
+countDown(10);
+
+
